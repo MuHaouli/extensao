85d94c89a7318da1ebf7f08143d04a0578257e1f
Murilo Haouli <murilo.haouli@edu.unifil.br>
2026-05-20 21:20:09 -0300

    update login settings

--- iterrogatio/iterrogatio/settings.py
+++ iterrogatio/iterrogatio/settings.py
@@ -153,9 +153,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'login'
+LOGIN_URL = 'login_api'
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = '/'
 
 # Dev: permitir frontend React (create-react-app)
 CORS_ALLOWED_ORIGINS = [
